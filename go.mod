4648abee0498f8b995bbfa3fdf7c41cc17b2f5c5
parikxxit <singhp154154@gmail.com>
2023-04-15 14:47:23 +0530

    reading test from file

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/parikxxit/ardanlab-nlp
 
 go 1.20
 
-require github.com/stretchr/testify v1.8.2
+require (
+	github.com/BurntSushi/toml v1.2.1
+	github.com/stretchr/testify v1.8.2
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
--- nlp_test.go
+++ nlp_test.go
@@ -1,10 +1,40 @@
 package nlp
 
 import (
+	"fmt"
+	"io/ioutil"
 	"testing"
 
+	"github.com/BurntSushi/toml"
 	"github.com/stretchr/testify/require"
 )
+
+type tokenizeTestCase struct {
+	text   string   `toml:"text"`
+	tokens []string `toml:"tokens"`
+}
+type tokenizeTestCases struct {
+	cases []tokenizeTestCase `toml:"cases"`
+}
+
+func loadTokenizeTestCase(t *testing.T) []tokenizeTestCase {
+	data, err := ioutil.ReadFile("tokenize_cases.toml")
+	require.NoError(t, err, "Read File")
+	var tc tokenizeTestCases
+	err = toml.Unmarshal(data, &tc)
+	require.NoError(t, err, "Unmarhaling")
+	fmt.Printf("%#v", tc)
+	return tc.cases
+}
+
+func TestTokenizeTable(t *testing.T) {
+	for _, tc := range loadTokenizeTestCase(t) {
+		t.Run(tc.text, func(t *testing.T) {
+			tokens := Tokenize(tc.text)
+			require.Equal(t, tokens, tc.tokens)
+		})
+	}
+}
 
 func TestTokenize(t *testing.T) {
 	text := "some new input"
