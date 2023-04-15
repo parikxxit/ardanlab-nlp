4d0b90faa71d0c5242bde90d28e3795c6080334b
parikxxit <singhp154154@gmail.com>
2023-04-15 13:28:05 +0530

    added testify for testing

--- nlp_test.go
+++ nlp_test.go
@@ -1,15 +1,19 @@
 package nlp
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestTokenize(t *testing.T) {
 	text := "some new input"
 	gotTokens := Tokenize(text)
 	expectedTokens := []string{"some", "new", "input"}
+	require.Equal(t, expectedTokens, gotTokens)
+	/* before testify
 	if !reflect.DeepEqual(expectedTokens, gotTokens) {
 		t.Fatalf("Expected %#v got %#v", expectedTokens, gotTokens)
 	}
+	*/
 }
